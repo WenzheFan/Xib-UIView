140e009757a4689e86c62eb8095c71c0d598cd66
fanwenzhe <523935688@qq.com>
2018-03-16 17:03:54 +0800

    Initial Commit

---
+++ Xib创建UIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xib创建UIView
+//
+//  Created by apple on 2018/3/16.
+//  Copyright © 2018年 范文哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Xib创建UIView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Xib创建UIView
+//
+//  Created by apple on 2018/3/16.
+//  Copyright © 2018年 范文哲. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
